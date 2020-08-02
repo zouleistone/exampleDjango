39b1cc95e4f5ca4965c60354004c4c9ed02a5aef
zouleistone <1195564225@qq.com>
2020-08-03 02:42:57 +0800

    creat project

--- exampleDjango/urls.py
+++ exampleDjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import example.views as ev
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', ev),
 ]
