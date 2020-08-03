5116c7d4522308d6e628f611794c5b2dbef1941d
zouleistone <1195564225@qq.com>
2020-08-04 01:40:27 +0800

    creat project

--- exampleDjango/settings.py
+++ exampleDjango/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'example',
-    'demo',
+    'demo'
 ]
 
 MIDDLEWARE = [
--- exampleDjango/urls.py
+++ exampleDjango/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import example.views as ev
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', ev),
+    path('example/', include('example.urls')),
+    path('demo/', include('demo.urls')),
 ]
